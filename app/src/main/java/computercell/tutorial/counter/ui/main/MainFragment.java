4993af9fb8575aa72f51445ff1dc4510ec5fd1de
QuantumNovice <portabl3lapy@gmail.com>
2020-02-18 20:42:06 +0500

    app/src/main/java/computercell/tutorial/counter/ui/main/MainFragment.java

---
+++ app/src/main/java/computercell/tutorial/counter/ui/main/MainFragment.java
@@ -0,0 +1,51 @@
+package computercell.tutorial.counter.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import computercell.tutorial.counter.R;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    public int x = 0;
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+
+
+
+    /** Called when the user touches the button */
+
+
+    public void sendMessage(View v) {
+        // implements your things
+
+    }
+}
